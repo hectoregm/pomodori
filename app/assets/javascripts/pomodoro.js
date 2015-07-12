186304c3a1771ed8c9a4725d5f8d42c093486084
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-07-12 12:39:54 -0500

    Refactoring of pomodoro module.

--- app/assets/javascripts/pomodoro.js
+++ app/assets/javascripts/pomodoro.js
@@ -1,32 +1,43 @@
 pomodori.pomodoro = (function () {
-  var start_time, state, end_time, total;
+  var start_time, state, end_time, total, pomodoro_time, break_time;
   var timer = document.querySelector('.soon');
   var alarm = document.querySelector('.alarm');
   var title = document.querySelector('.title');
   var start = document.querySelector('.start');
 
-  var init = function () {
-    console.log("In init");
-    start_time = new Date();
-    state = "work";
+  var init = function (testing) {
+    if (testing) {
+      break_time = pomodoro_time = 'in 10 seconds';
+    } else {
+      pomodoro_time = 'in 25 minutes';
+      break_time = 'in 5 minutes';
+    }
     total = 0;
 
     $(start).on('click', function () {
-      console.log("Clicked Start button");
-      start();
+      startWork();
     });
+
+    startWork();
   };
 
-  var start = function () {
+  var startWork = function () {
     start_time = new Date();
     state = "work";
 
     title.innerHTML = "Pomodoro";
-    Soon.setOption(timer, 'due', 'in 10 seconds');
+    Soon.setOption(timer, 'due', pomodoro_time);
+    $(start).addClass("hide");
   };
 
+  var startBreak = function () {
+    state = "break";
+
+    title.innerHTML = "Break";
+    Soon.setOption(timer, 'du', break_time);
+  }
+
   var completed = function () {
-    console.log("In completed");
     end_time = new Date();
     alarm.play();
 
@@ -37,13 +48,9 @@ pomodori.pomodoro = (function () {
         dataType: "json",
         data: { pomodoro: { started_at: start_time , completed_at: end_time, length: 25 } },
         success: function (data) {
-          console.log("Successful AJAX call.");
           console.log(data);
-
-          console.log("Start of break");
-          state = "break";
-          title.innerHTML = "Break";
-          Soon.setOption(timer, 'due', 'in 10 seconds');
+          total += 1;
+          startBreak();
         },
         error: function(data) {
           console.log("Error in AJAX call.");
@@ -51,8 +58,7 @@ pomodori.pomodoro = (function () {
         }
       });
     } else if (state === "break") {
-      console.log("End of break");
-      $('.start').removeClass("hide");
+      $(start).removeClass("hide");
       title.innerHTML = "Continue ?";
     }
   };
