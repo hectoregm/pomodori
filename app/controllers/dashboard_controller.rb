c82eb2783aa0c0419e2f7f772c509f23e959ecf9
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-08-04 14:53:26 -0500

    Added basic dashboard with the number of pomodori for the day and week

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,14 @@
 class DashboardController < ApplicationController
   def index
+    Time.use_zone("America/Mexico_City") do
+      @total_today = Pomodoro.today.count
+      @total_week = Pomodoro.by_week.count
+    end
   end
 
   def today
+  end
+
+  def week
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'dashboard/index'
   get 'dashboard/today'
+  get 'dashboard/week'
 
   resources :tasks do
     resources :pomodori, only: [:new, :show, :create, :destroy]
