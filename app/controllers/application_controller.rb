dc4867d09be8a8f5f8a1404621856d63de289879
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-08-14 14:27:06 -0500

    Add TODO note for time_zone around_filter.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   around_filter :time_zone
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def time_zone(&block)
-    Time.use_zone("America/Mexico_City", &block)
+    # TODO: When adding users use the user prefered time zone
+    # instead of this hard coded zone.
+    Time.use_zone('America/Mexico_City', &block)
   end
 end
