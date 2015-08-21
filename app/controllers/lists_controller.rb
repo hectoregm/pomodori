4ef50c0444dfe7117147a971ce4ca08debfa766a
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-08-20 19:47:43 -0500

    Add basic skeleton for ListsController.

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,10 @@
+class ListsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'lists/index'
+
+  get 'lists/create'
+
+  get 'lists/destroy'
+
   get 'dashboard/index'
   get 'dashboard/today'
   get 'dashboard/week'
