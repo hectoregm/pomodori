56e606d0d95f31312e2ff7858b91f6ac598fd89a
Hector E. Gomez Morales <hectoregm@gmail.com>
2015-09-04 21:15:46 -0500

    Add progress bar for each project.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,23 @@ class Project < ActiveRecord::Base
   validates_presence_of :name
 
   has_many :tasks
+
+  def tasks_not_done
+    tasks.where(done: false)
+  end
+
+  def progress
+    total_estimate = 0
+    total_done = 0
+    tasks_not_done.each do |task|
+      total_estimate += task.estimate
+      total_done += task.pomodori.count
+    end
+
+    if total_estimate.zero?
+      100
+    else
+      (total_done / total_estimate.to_f) * 100
+    end
+  end
 end
