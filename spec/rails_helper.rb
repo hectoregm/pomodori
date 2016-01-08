9c26441501b8b9baa0ba2b19aa21660b73b05e1f
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-01-07 18:45:30 -0800

    Update Gemfile gems. Up to rails 4.2.5

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-require 'shoulda/matchers'
+require 'shoulda_helper'
 require 'capybara/rails'
 require 'capybara/poltergeist'
 
