13fbdab6e2a20c7640b64eef305a2cb5402a3646
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-01-07 20:05:17 -0800

    Added test for today#show action

--- spec/controllers/today_controller_spec.rb
+++ spec/controllers/today_controller_spec.rb
@@ -6,5 +6,15 @@ RSpec.describe TodayController, type: :controller do
       get :show
       expect(response).to have_http_status(:success)
     end
+
+    it 'loads all of the posts into @posts' do
+      task_one = FactoryGirl.create(:task, today: true)
+      FactoryGirl.create(:task, today: false)
+      task_two = FactoryGirl.create(:task, today: true)
+      FactoryGirl.create(:task, today: true, done: true)
+      get :show
+
+      expect(assigns(:tasks)).to match_array([task_one, task_two])
+    end
   end
 end
